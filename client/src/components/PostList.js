c560d4f4bb3b1bc652d96c0d59f3f62fda40f76f
akhilkatti <akhilkatti.99@gmail.com>
2021-10-23 23:37:59 +0530

    client side initial setup complete

---
+++ client/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { List, Datagrid, TextField, DateField, EditButton, DeleteButton} from 'react-admin'; 
+
+
+const PostList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PostList;
