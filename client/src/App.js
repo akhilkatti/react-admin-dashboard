43ca8c2a042ed1a182bc6aa9440639d0803333be
akhilkatti <akhilkatti.99@gmail.com>
2021-11-01 19:41:12 +0530

    User Create feat. added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import PostList from './components/PostList';
 import PostCreate from './components/PostCreate';
 import PostEdit from './components/PostEdit';
 import UserList from './components/UserList';
-// import UserCreate from './components/UserCreate';
+import UserCreate from './components/UserCreate';
 // import UserEdit from './components/UserEdit';
 
 
@@ -13,9 +13,9 @@ function App() {
   return (
     <Admin dataProvider={restProvider('http://localhost:3000')}>
       <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
-      <Resource name='users' list={UserList}  />
+      <Resource name='users' list={UserList} create={UserCreate} />
     </Admin>
   );
 }
-// create={UserCreate} edit={UserEdit}
+//  edit={UserEdit}
 export default App;
