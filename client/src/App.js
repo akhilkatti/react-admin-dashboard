91042b1e3664aea6b2b56e66a9de1ceb27a943eb
akhilkatti <akhilkatti.99@gmail.com>
2021-11-01 19:35:25 +0530

    User list added

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import restProvider from 'ra-data-simple-rest';
 import PostList from './components/PostList';
 import PostCreate from './components/PostCreate';
 import PostEdit from './components/PostEdit';
+import UserList from './components/UserList';
+// import UserCreate from './components/UserCreate';
+// import UserEdit from './components/UserEdit';
+
 
 function App() {
   return (
     <Admin dataProvider={restProvider('http://localhost:3000')}>
       <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
+      <Resource name='users' list={UserList}  />
     </Admin>
   );
 }
-
+// create={UserCreate} edit={UserEdit}
 export default App;
